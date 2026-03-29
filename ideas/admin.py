e5a761f4677e2ec04f675f22f50023618d0743bb
unknown <ibrahiembaraa@gmail.com>
2026-03-29 21:11:30 +0300

    bootcamp

--- ideas/admin.py
+++ ideas/admin.py
@@ -2,9 +2,7 @@ from django.contrib import admin
 from .models import Season,Idea,IdeaForm,FormQuestion,FormQuestionChoice,TeamMember
 from .phases import SeasonPhase
 
-@admin.register(SeasonPhase)
-class SeasonPhaseAdmin(admin.ModelAdmin):
-    list_display = ['id', 'season', 'phase', 'start_date', 'end_date']
+admin.site.register(SeasonPhase)
 admin.site.register(Season)
 admin.site.register(Idea)
 admin.site.register(IdeaForm)
