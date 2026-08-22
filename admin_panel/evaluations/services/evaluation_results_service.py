bc83ce3c4abdd88cddca6e876a9bfcb1656aaa31
BaraaEBrahiem <ibrahiembaraa@gmail.com>
2026-08-22 15:11:35 +0300

    evaluation table

--- admin_panel/evaluations/services/evaluation_results_service.py
+++ admin_panel/evaluations/services/evaluation_results_service.py
@@ -50,8 +50,13 @@ class EvaluationResultsService:
     def get_results(*, sector=None, status=None):
 
         ideas = Idea.objects.select_related(
-            "owner"
-        )
+    "owner"
+).filter(
+    status__in=[
+        IdeaStatus.EVALUATION,
+        IdeaStatus.EVALUATED,
+    ]
+)
 
         if sector:
             ideas = ideas.filter(
