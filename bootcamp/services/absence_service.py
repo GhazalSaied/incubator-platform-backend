07b7f8988b246a832170fbf12dcd40db786b32a1
BaraaEBrahiem <ibrahiembaraa@gmail.com>
2026-08-22 12:59:29 +0300

    absence request

--- bootcamp/services/absence_service.py
+++ bootcamp/services/absence_service.py
@@ -1,7 +1,6 @@
 from bootcamp.models import BootcampAbsenceRequest
 from ideas.services.idea_service import IdeaService
 from rest_framework.exceptions import ValidationError
-
 class AbsenceService:
 
     @staticmethod
@@ -17,8 +16,18 @@ class AbsenceService:
         if existing:
             raise ValidationError("تم تقديم طلب غياب مسبقاً لهذه الجلسة")
 
-        return BootcampAbsenceRequest.objects.create(
+        absence_request = BootcampAbsenceRequest.objects.create(
             idea=idea,
             session_id=session_id,
             reason=reason
         )
+
+        EventBus.emit(
+            "bootcamp_absence_request_submitted",
+            {
+                "absence_request": absence_request,
+                "actor": user,
+            }
+        )
+
+        return absence_request
