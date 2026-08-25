c7c74eb1cbfa5debc433c6a0759cce068175d8ae
BaraaEBrahiem <ibrahiembaraa@gmail.com>
2026-08-25 10:33:07 +0300

    nearest workshops

--- ideas/serializers.py
+++ ideas/serializers.py
@@ -351,8 +351,7 @@ class PublicExhibitionListSerializer(serializers.Serializer):
     owner = serializers.SerializerMethodField()
     image = serializers.SerializerMethodField()
     def get_title(self, obj):
-        return obj.data.get("title")
-    
+        return obj.project.title or ""
 
     def get_image(self, obj):
         image = (obj.data.get("صورة المشروع")
